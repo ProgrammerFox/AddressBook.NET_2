00ae2aad21cc43f48e4475fdc37edb76b41b31a8
ProgrammerFox <foxcoder12@gmail.com>
2022-12-20 23:45:19 +0200

    Added person and address book classes.

---
+++ AddressBook.NET_2/Model/AddressBook.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook_dotnet6.Model
+{
+    public class AddressBook<T> : List<T> 
+        where T : BasePerson
+    {
+        public void Swap(int id1, int id2)
+        {
+            (this[id1], this[id2]) = (this[id2], this[id1]);
+        }
+    }
+}
