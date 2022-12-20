6d005a32c6abf92b8590ac429072839ff175244e
ProgrammerFox <foxcoder12@gmail.com>
2022-12-21 00:53:49 +0200

    Added readme file. Some edits in code.

--- AddressBook.NET_2/Program.cs
+++ AddressBook.NET_2/Program.cs
@@ -1,6 +1,14 @@
 ﻿using AddressBook_dotnet6.Model;
 using AddressBook_Dotnet6.View;
 
+Console.WriteLine(@"Address Book by
+ ____                                                                       _____              
+|  _ \  _ __   ___    __ _  _ __   __ _  _ __ ___   _ __ ___    ___  _ __  |  ___|  ___  __  __
+| |_) || '__| / _ \  / _` || '__| / _` || '_ ` _ \ | '_ ` _ \  / _ \| '__| | |_    / _ \ \ \/ /
+|  __/ | |   | (_) || (_| || |   | (_| || | | | | || | | | | ||  __/| |    |  _|  | (_) | >  < 
+|_|    |_|    \___/  \__, ||_|    \__,_||_| |_| |_||_| |_| |_| \___||_|    |_|     \___/ /_/\_\
+                     |___/                                                                     ");
+
 var addressBookCommands = new AddressBookCommands<BasePerson>(new AddressBook<BasePerson>(), new ConsoleCommunicator());
 
 var commander = addressBookCommands.GenerateCommander();
---
+++ README.md
@@ -0,0 +1,9 @@
+# AddressBook C Sharp
+
+Refactored version of the address book, taking into account the purity of the code (sonar), solid pattern.
+
+## Notes
+
+- IDE: [Visual Studio Community](https://visualstudio.microsoft.com/ru/vs/community/)
+- ASCII styled text: [ASCII Art Library For Python](https://pypi.org/project/art/)
+- Task taken from the book "Bite of Python"
